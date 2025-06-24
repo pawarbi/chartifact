00d5d5426841e791f6276d467d56be349f4129fc
Dan Marshall <danmar@microsoft.com>
2025-06-24 10:16:20 -0700

    Vite configuration for dual output

--- packages/host/vite.bundle.config.js
+++ packages/host/vite.bundle.config.js
@@ -2,27 +2,36 @@ import { defineConfig } from 'vite';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
-export default defineConfig({
-  build: {
+const commonOutputConfig = {
+  format: 'umd',
+  name: 'IDocsHost',
+  globals: {
+    'markdown-it': 'markdownit',
+    'vega': 'vega',
+    'vega-lite': 'vegaLite',
+  },
+  entryFileNames: 'idocshost.umd.js',
+};
+
+export default defineConfig({  build: {
     lib: {
-      entry: './src/index.ts',  // Path to the entry file for the bundle
-      name: 'IDocsHost',
-      fileName: 'idocshost',  // Base name for output files; Vite will add format extensions automatically
-      formats: ['umd'],    // UMD format for browser compatibility
+      entry: './src/index.ts',
     },
-    outDir: '../../docs/host',  // Output directory for the bundle
     minify: false,
+    emptyOutDir: false,
     rollupOptions: {
       // External dependencies that the library expects consumers to provide
       external: ['markdown-it', 'vega', 'vega-lite', 'tabulator-tables'],
-      output: {
-        globals: {
-          'markdown-it': 'markdownit',
-          'vega': 'vega',
-          'vega-lite': 'vegaLite',
+      output: [
+        {
+          ...commonOutputConfig,
+          dir: '../../docs/host',
         },
-        entryFileNames: 'idocshost.umd.js',
-      },
+        {
+          ...commonOutputConfig,
+          dir: '../vscode/resources',
+        },
+      ],
       plugins: [
         resolve(),   // Resolves Node modules
         commonjs(),  // Converts CommonJS to ES6
