f9dcdc77b027e3c69fca6bdbfa1aa2c88e0c517e
Dan Marshall <danmar@microsoft.com>
2025-06-25 09:41:33 -0700

    multiple outputs

--- packages/renderer/vite.bundle.config.js
+++ packages/renderer/vite.bundle.config.js
@@ -2,27 +2,36 @@ import { defineConfig } from 'vite';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
+const commonOutputConfig = {
+  format: 'umd',
+  name: 'IDocs',
+  globals: {
+    'markdown-it': 'markdownit',
+    'vega': 'vega',
+    'vega-lite': 'vegaLite',
+  },
+  entryFileNames: 'idocs.umd.js',
+};
+
 export default defineConfig({
   build: {
     lib: {
       entry: './dist/es2022/index.js',  // Path to the entry file for the bundle
-      name: 'IDocs',
-      fileName: 'idocs',  // Base name for output files; Vite will add format extensions automatically
-      formats: ['umd'],    // UMD format for browser compatibility
     },
-    outDir: './dist/umd',  // Output directory for the bundle
     minify: false,
     rollupOptions: {
       // External dependencies that the library expects consumers to provide
       external: ['markdown-it', 'vega', 'vega-lite', 'tabulator-tables'],
-      output: {
-        globals: {
-          'markdown-it': 'markdownit',
-          'vega': 'vega',
-          'vega-lite': 'vegaLite',
+      output: [
+        {
+          ...commonOutputConfig,
+          dir: './dist/umd',
         },
-        entryFileNames: 'idocs.umd.js',
-      },
+        {
+          ...commonOutputConfig,
+          dir: '../vscode/resources',
+        },
+      ],
       plugins: [
         resolve(),   // Resolves Node modules
         commonjs(),  // Converts CommonJS to ES6
