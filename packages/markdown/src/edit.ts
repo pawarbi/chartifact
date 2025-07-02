f476119dbf7cde3c1bcd6cd5795d2fa470dfdae9
Dan Marshall <danmar@microsoft.com>
2025-07-02 16:46:41 -0700

    move editor to module

---
+++ packages/markdown/src/edit.ts
@@ -0,0 +1,15 @@
+import { Renderer, RendererOptions } from "./renderer.js";
+
+export function bindTextarea(textarea: HTMLTextAreaElement, outputElement: HTMLElement, options?: RendererOptions) {
+    const renderer = new Renderer(outputElement, options);
+
+    const render = () => {
+        const content = textarea.value;
+        renderer.render(content);
+    };
+
+    textarea.addEventListener('input', render);
+    // Initial render
+    render();
+    return renderer;
+}
