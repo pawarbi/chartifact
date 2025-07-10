3c270b262db58dc6d6789b41b01f7669c0bcadda
Dan Marshall <danmar@microsoft.com>
2025-07-10 14:51:16 -0700

    add iframe

--- packages/editor/src/editor.tsx
+++ packages/editor/src/editor.tsx
@@ -1,10 +1,32 @@
+import { InteractiveDocument } from "schema";
+import { CustomWindow, Iframe } from "./iframe.js";
+
+
 export interface Props {
 }
 
-export function Editor (props: Props)  {
+export function Editor(props: Props) {
+    const page: InteractiveDocument = {
+        title: "Sample Page",
+        layout: {
+            css: "",
+        },
+        dataLoaders: [],
+        groups: [],
+        variables: [],
+    };
+    const stateRef = React.useRef<CustomWindow>(null);
     return (
         <div>
-            Hello world
+            <div>
+                Hello world
+            </div>
+            <div>
+                <Iframe
+                    page={page}
+                    stateRef={stateRef}
+                />
+            </div>
         </div>
     );
 }
