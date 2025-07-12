b084cbae5b5b847187d088db2a2d7b75187b120a
Dan Marshall <danmar@microsoft.com>
2025-07-12 14:44:07 -0700

    add sandbox package

---
+++ packages/sandbox/src/index.ts
@@ -0,0 +1,54 @@
+import { rendererHtml } from "./resources/rendererHtml";
+import { rendererUmdJs } from "./resources/rendererUmdJs";
+
+interface SandboxOptions {
+    onReady?: () => void;
+    onError?: (error: Event) => void;
+    markdown?: string;
+}
+
+export class Sandbox {
+    private iframe: HTMLIFrameElement;
+
+    constructor(element: HTMLElement, options?: SandboxOptions) {
+        const { iframe, blobUrl } = createIframe(options?.markdown);
+        this.iframe = iframe;
+        element.appendChild(this.iframe);
+
+        this.iframe.addEventListener('load', () => {
+            URL.revokeObjectURL(blobUrl);
+            options?.onReady?.();
+        });
+
+        this.iframe.addEventListener('error', (error) => {
+            console.error("Error loading iframe:", error);
+            URL.revokeObjectURL(blobUrl);
+            options?.onError?.(error);
+        });
+    }
+
+    send(markdown: string): void {
+        this.iframe.contentWindow?.postMessage({ markdown }, '*');
+    }
+}
+
+export function createIframe(markdown?: string) {
+    const title = 'Interactive Document Sandbox';
+    const html = rendererHtml
+        .replace('{{MARKDOWN_CONTENT}}', () => markdown || '')
+        .replace('{{TITLE}}', () => title)
+        .replace('{{RENDERER_SCRIPT}}', () => `<script>${rendererUmdJs}</script>`);
+
+    const htmlBlob = new Blob([html], { type: 'text/html' });
+    const blobUrl = URL.createObjectURL(htmlBlob);
+
+    const iframe = document.createElement('iframe');
+    iframe.sandbox = 'allow-scripts';
+    iframe.src = blobUrl;
+    iframe.style.width = '100%';
+    iframe.style.height = '800px';
+    iframe.style.border = 'none';
+    iframe.title = title;
+
+    return { iframe, blobUrl };
+}
---
+++ packages/sandbox/vite.bundle.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+const commonOutputConfig = {
+  format: 'umd',
+  name: 'IDocs.sandbox',
+  globals: {
+    'markdown-it': 'markdownit',
+    'vega': 'vega',
+    'vega-lite': 'vegaLite',
+  },
+  entryFileNames: 'idocs.sandbox.umd.js',
+};
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: './src/index.ts',
+    },
+    minify: false,
+    emptyOutDir: false,
+    rollupOptions: {
+      // External dependencies that the library expects consumers to provide
+      external: ['markdown-it', 'vega', 'vega-lite', 'tabulator-tables'],
+      output: [
+        {
+          ...commonOutputConfig,
+          dir: './dist/umd',
+        },
+      ],
+      plugins: [
+        resolve(),   // Resolves Node modules
+        commonjs(),  // Converts CommonJS to ES6
+      ],
+    },
+  },
+});
