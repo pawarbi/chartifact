a8285134354a30485d25150cceee7e30c67ba451
Dan Marshall <danmar@microsoft.com>
2025-07-21 11:01:41 -0700

    remove markdown namespace

--- packages/compiler/umd.ts
+++ packages/compiler/umd.ts
@@ -1,4 +1,3 @@
 export * as common from 'common';
 export * as compiler from './src/index.js';
-export * as markdown from '@microsoft/interactive-document-markdown';
 export type * as schema from 'schema';
