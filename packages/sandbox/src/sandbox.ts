31a44d6f218499aa7e73b86dac8ebff7c0b52cf7
Dan Marshall <danmar@microsoft.com>
2025-07-23 23:14:25 -0700

    remove markdownit, add csstree

--- packages/sandbox/src/sandbox.ts
+++ packages/sandbox/src/sandbox.ts
@@ -42,6 +42,7 @@ export class Sandbox extends Previewer {
         return `
 <link href="https://unpkg.com/tabulator-tables@6.3.0/dist/css/tabulator.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/markdown-it/dist/markdown-it.min.js"></script>
+<script src="https://unpkg.com/css-tree/dist/csstree.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/vega@5.29.0"></script>
 <script src="https://cdn.jsdelivr.net/npm/vega-lite@5.20.1"></script>
 <script src="https://unpkg.com/tabulator-tables@6.3.0/dist/js/tabulator.min.js"></script>
