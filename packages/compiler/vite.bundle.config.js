bf91027e928b51a66cd459658495f7789d395660
Dan Marshall <danmar@microsoft.com>
2025-07-26 12:29:23 -0700

    suppress emptyoutdir message

--- packages/compiler/vite.bundle.config.js
+++ packages/compiler/vite.bundle.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
       entry: './umd.ts',
     },
     minify: false,
+    emptyOutDir: false,
     rollupOptions: {
       // External dependencies that the library expects consumers to provide
       external: ['vega', 'vega-lite'],
