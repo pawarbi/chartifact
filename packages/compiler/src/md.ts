0cf3264ded497c731ac1923f4146165fa1f0fc7a
Dan Marshall <danmar@microsoft.com>
2025-07-28 18:26:41 -0700

    normalize spec creation

--- packages/compiler/src/md.ts
+++ packages/compiler/src/md.ts
@@ -1,6 +1,6 @@
 import { Spec as VegaSpec } from 'vega-typings';
 import { TopLevelSpec as VegaLiteSpec } from "vega-lite";
-import { ChartFull, DataSource, ElementGroup, InteractiveDocument, TableElement, TextboxElement, Variable } from 'schema';
+import { ChartFull, DataSource, ElementGroup, InteractiveDocument, TableElement, Variable } from 'schema';
 import { getChartType } from './util.js';
 import { addDynamicDataLoaderToSpec, addStaticDataLoaderToSpec } from './loader.js';
 import { Plugins } from '@microsoft/interactive-document-markdown';
@@ -38,7 +38,7 @@ export function targetMarkdown(page: InteractiveDocument) {
         mdSections.push(tickWrap('css', page.layout.css));
     }
 
-    const tableElements = page.groups.flatMap(group => group.elements.filter(e => typeof e !== 'string' && e.type === 'table') as TableElement[]);
+    const tableElements = page.groups.flatMap(group => group.elements.filter(e => typeof e !== 'string' && e.type === 'table'));
 
     const vegaScope = dataLoaderMarkdown(dataLoaders.filter(dl => dl.type !== 'spec'), variables, tableElements);
 
@@ -91,7 +91,8 @@ function groupMarkdown(group: ElementGroup, variables: Variable[], vegaScope: Ve
         } else if (typeof element === 'object') {
             switch (element.type) {
                 case 'chart': {
-                    const chartFull = element.chart as ChartFull;
+                    const { chart } = element;
+                    const chartFull = chart as ChartFull;
                     //see if it's a placeholder or a full chart
                     if (!chartFull.spec) {
                         //add a markdown element (not a chart element) with an image of the spinner at /img/chart-spinner.gif
@@ -102,60 +103,65 @@ function groupMarkdown(group: ElementGroup, variables: Variable[], vegaScope: Ve
                     break;
                 }
                 case 'checkbox': {
+                    const { label, variableId } = element;
                     const cbSpec: Plugins.CheckboxSpec = {
-                        variableId: element.variableId,
-                        value: variables.find(v => v.variableId === element.variableId)?.initialValue as boolean,
-                        label: element.label,
+                        variableId: variableId,
+                        value: variables.find(v => v.variableId === variableId)?.initialValue as boolean,
+                        label,
                     };
                     mdElements.push(jsonWrap('checkbox', JSON.stringify(cbSpec, null, 2)));
                     break;
                 }
                 case 'dropdown': {
+                    const { label, variableId, options, dynamicOptions, multiple, size } = element;
                     const ddSpec: Plugins.DropdownSpec = {
-                        variableId: element.variableId,
-                        value: variables.find(v => v.variableId === element.variableId)?.initialValue as string | string[],
-                        label: element.label,
+                        variableId,
+                        value: variables.find(v => v.variableId === variableId)?.initialValue as string | string[],
+                        label,
                     };
-                    if (element.dynamicOptions) {
+                    if (dynamicOptions) {
                         ddSpec.dynamicOptions = {
-                            dataSourceName: element.dynamicOptions.dataSourceName,
-                            fieldName: element.dynamicOptions.fieldName,
+                            dataSourceName: dynamicOptions.dataSourceName,
+                            fieldName: dynamicOptions.fieldName,
                         };
                     } else {
-                        ddSpec.options = element.options;
+                        ddSpec.options = options;
                     }
-                    if (element.multiple) {
-                        ddSpec.multiple = element.multiple;
-                        ddSpec.size = element.size || 1;
+                    if (multiple) {
+                        ddSpec.multiple = multiple;
+                        ddSpec.size = size || 1;
                     }
 
                     mdElements.push(jsonWrap('dropdown', JSON.stringify(ddSpec, null, 2)));
                     break;
                 }
                 case 'image': {
-                    const urlSignal = vegaScope.createUrlSignal(element.urlRef);
+                    const { urlRef, alt, width, height } = element;
+                    const urlSignal = vegaScope.createUrlSignal(urlRef);
                     const imageSpec: Plugins.ImageSpec = {
                         srcSignalName: urlSignal.name,
-                        alt: element.alt,
-                        width: element.width,
-                        height: element.height,
+                        alt,
+                        width,
+                        height,
                     };
                     mdElements.push(jsonWrap('image', JSON.stringify(imageSpec, null, 2)));
                     break;
                 }
                 case 'presets': {
-                    const presetsSpec: Plugins.PresetsSpec = element.presets;
+                    const { presets } = element;
+                    const presetsSpec: Plugins.PresetsSpec = presets;
                     mdElements.push(jsonWrap('presets', JSON.stringify(presetsSpec, null, 2)));
                     break;
                 }
                 case 'slider': {
+                    const { label, min, max, step, variableId } = element;
                     const sliderSpec: Plugins.SliderSpec = {
-                        variableId: element.variableId,
-                        value: variables.find(v => v.variableId === element.variableId)?.initialValue as number,
-                        label: element.label,
-                        min: element.min,
-                        max: element.max,
-                        step: element.step,
+                        variableId,
+                        value: variables.find(v => v.variableId === variableId)?.initialValue as number,
+                        label,
+                        min,
+                        max,
+                        step,
                     };
                     mdElements.push(jsonWrap('slider', JSON.stringify(sliderSpec, null, 2)));
                     break;
@@ -167,12 +173,13 @@ function groupMarkdown(group: ElementGroup, variables: Variable[], vegaScope: Ve
                     break;
                 }
                 case 'textbox': {
-                    const textboxElement = element as TextboxElement;
+                    const { variableId, label, multiline, placeholder } = element;
                     const textboxSpec: Plugins.TextboxSpec = {
-                        variableId: textboxElement.variableId,
-                        value: variables.find(v => v.variableId === textboxElement.variableId)?.initialValue as string,
-                        label: textboxElement.label,
-                        multiline: textboxElement.multiline,
+                        variableId,
+                        value: variables.find(v => v.variableId === variableId)?.initialValue as string,
+                        label,
+                        multiline,
+                        placeholder,
                     };
                     mdElements.push(jsonWrap('textbox', JSON.stringify(textboxSpec, null, 2)));
                     break;
