4216135f5e0a54271976bbd5a6053d989fb95b24
Dan Marshall <danmar@microsoft.com>
2025-08-01 13:20:34 -0700

    remove copyright headers

--- packages/schema/scripts/strip-blanks.js
+++ packages/schema/scripts/strip-blanks.js
@@ -5,13 +5,24 @@ const filePath = 'dist/idoc.d.ts';
 // Read the file content
 const content = fs.readFileSync(filePath, 'utf8');
 
+const copyrightHeader = `/**
+* Copyright (c) Microsoft Corporation.
+* Licensed under the MIT License.
+*/`;
+
+//remove all instances of the copyright header
+const updatedContent = content.split(copyrightHeader).join('');
+
 // Split by lines, filter out empty lines, and rejoin
-const strippedContent = content
+const strippedContent = updatedContent
   .split(/\r?\n/)
   .filter(line => line.trim())
   .join('\n');
 
+//add a single copyright header at the top
+const finalContent = `${copyrightHeader}\n${strippedContent}`;
+
 // Write the stripped content back to the file
-fs.writeFileSync(filePath, strippedContent);
+fs.writeFileSync(filePath, finalContent, 'utf8');
 
 console.log('Blank lines stripped from', filePath);
