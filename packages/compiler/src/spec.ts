06a4d37bb0f7f9e97942179532881cbab8540eff
Dan Marshall <danmar@microsoft.com>
2025-08-02 19:49:03 -0700

    fix: add description to the VegaSpec in createSpecWithVariables function

--- packages/compiler/src/spec.ts
+++ packages/compiler/src/spec.ts
@@ -14,6 +14,7 @@ export function createSpecWithVariables(variables: Variable[], tableElements: Ta
     //preload with variables as signals
     const spec: VegaSpec = {
         $schema: "https://vega.github.io/schema/vega/v5.json",
+        description: "This is the central brain of the page",
         signals: [],
         data: [],
     };
