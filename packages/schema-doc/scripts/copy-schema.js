6dc6bb3dbc9bc42e615672b56cf33215bbeb6c75
Dan Marshall <danmar@microsoft.com>
2025-08-04 09:38:13 -0700

    rename schema to schema-doc

--- packages/schema-doc/scripts/copy-schema.js
+++ packages/schema-doc/scripts/copy-schema.js
@@ -1,8 +1,4 @@
 import fs from 'fs';
-import path from 'path';
-
-// Create the schema directory if it doesn't exist
-fs.mkdirSync('../../docs/schema', { recursive: true });
 
 // Copy the schema file
 fs.copyFileSync('dist/idoc.schema.json', '../../docs/schema/idoc_v1.json');
