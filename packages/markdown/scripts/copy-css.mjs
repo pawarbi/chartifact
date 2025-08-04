f61e8a8dd693e43e03ed10f87622c8c4a893997d
Dan Marshall <danmar@microsoft.com>
2025-08-04 10:07:25 -0700

    add css reset

---
+++ packages/markdown/scripts/copy-css.mjs
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+// Define the source and destination directories
+const distDir = path.join(__dirname, '../dist/css');
+
+// Ensure the destination directories exist
+if (!fs.existsSync(distDir)) {  
+    fs.mkdirSync(distDir, { recursive: true });
+}
+
+// Copy the css reset file
+fs.copyFileSync('./chartifact-reset.css', './dist/css/chartifact-reset.css');
+fs.copyFileSync('./chartifact-reset.css', '../../docs/assets/css/chartifact-reset.css');
+
+console.log('CSS reset file copied successfully to ../../docs/assets/css');
