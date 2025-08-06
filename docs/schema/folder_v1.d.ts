806edc5c809c81b2f939a7d463e7f6da45b8d681
Dan Marshall <danmar@microsoft.com>
2025-08-05 21:48:05 -0700

    add folder schema

---
+++ docs/schema/folder_v1.d.ts
@@ -0,0 +1,25 @@
+/**
+* Copyright (c) Microsoft Corporation.
+* Licensed under the MIT License.
+*/
+interface DocRef {
+    title: string;
+    /** URL to the document, either absolute or relative to the folder location. */
+    href: string;
+}
+interface Folder {
+    title: string;
+    docs: DocRef[];
+}
+
+/**
+* Copyright (c) Microsoft Corporation.
+* Licensed under the MIT License.
+*/
+
+/** JSON Schema version with $schema property for validation */
+type FolderWithSchema = Folder & {
+    $schema?: string;
+};
+
+export type { DocRef, Folder, FolderWithSchema };
