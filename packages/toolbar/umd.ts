baca6f355d5e9bbd3c4f521cb058ffd50657f837
Dan Marshall <danmar@microsoft.com>
2025-08-25 13:31:43 -0700

    toolbar bundle

---
+++ packages/toolbar/umd.ts
@@ -0,0 +1,5 @@
+/**
+* Copyright (c) Microsoft Corporation.
+* Licensed under the MIT License.
+*/
+export * as toolbar from './src/index.js';
---
+++ packages/toolbar/vite.bundle.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+const commonOutputConfig = {
+  format: 'umd',
+  name: 'Chartifact',
+  extend: true,
+  globals: {
+    'vega': 'vega',
+    'vega-lite': 'vegaLite',
+  },
+  entryFileNames: 'chartifact.toolbar.umd.js',
+};
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: './umd.ts',
+    },
+    minify: false,
+    emptyOutDir: false,
+    rollupOptions: {
+      // External dependencies that the library expects consumers to provide
+      external: ['vega', 'vega-lite'],
+      output: [
+        {
+          ...commonOutputConfig,
+          dir: './dist/umd',
+        },
+        {
+          ...commonOutputConfig,
+          dir: '../../docs/dist/v1',
+        },
+      ],
+      plugins: [
+        resolve(),   // Resolves Node modules
+        commonjs(),  // Converts CommonJS to ES6
+      ],
+    },
+  },
+});
