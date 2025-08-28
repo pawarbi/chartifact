efd0385167a77c736ca656a2368e43a03dbedb87
Dan Marshall <danmar@microsoft.com>
2025-08-28 12:46:40 -0700

    Add Vega.js script to resource links in webview content

--- packages/vscode/src/web/command-preview.ts
+++ packages/vscode/src/web/command-preview.ts
@@ -216,6 +216,7 @@ function getWebviewContent(webView: vscode.Webview, context: vscode.ExtensionCon
 
 	// Build the resource links block
 	const resourceLinks = [
+		script(resourceUrl('vega.min.js')),
 		script(resourceUrl('chartifact.host.umd.js')),
 		script(resourceUrl('preview.js')),
 	].join('\n    ');
